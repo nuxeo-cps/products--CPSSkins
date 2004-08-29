1d5b4a505142c36fa279963b2d5714965ce7ae8f
jmorliaguet <devnull@localhost>
2004-08-29 15:02:59 +0000

    bug fix. 'context.aq_explicit' makes it impossible to obtain the 'visible_meth' which may be located in skins.

--- skins/CPSSkins/cpsskins_listEditCategories.py
+++ skins/CPSSkins/cpsskins_listEditCategories.py
@@ -2,15 +2,13 @@
 # category 'general' is by default
 categories = ['general']
 
-context = context.aq_explicit
-
 for propid in context.propertyIds():
     for obj in context.propertyMap():
         if obj['id'] == propid:
              visibility = obj.get('visible', None)
              is_visible = 0
              if visibility is not None:
-                 if hasattr(context, visibility):
+                 if getattr(context, visibility, None) is not None:
                      visible_meth = getattr(context, visibility)
                      if callable(visible_meth):
                          is_visible = apply(visible_meth, ())
@@ -23,7 +21,7 @@ for propid in context.propertyIds():
              if category not in categories and category is not 'none': 
                  categories.append(category)
 
-if getattr(context, 'isportletbox', None) is not None:
+if getattr(context.aq_explicit, 'isportletbox', None) is not None:
     if context.getPortletId() is not None:
         categories.append('Portlet')
 
