c35c2fafd5d4d69a4ff593a174969dbf773b328a
jmorliaguet <devnull@localhost>
2004-09-03 11:31:07 +0000

    added the script to delete unused portlets (portlets that reside in non-existent or in hidden slots)

---
+++ skins/cpsskins_cps3/cpsskins_delete_unused_portlets.py
@@ -0,0 +1,18 @@
+##parameters=theme=None, REQUEST=None
+
+ptltool = context.portal_cpsportlets
+cpsmcat = context.Localizer.default
+
+hidden_slots = context.cpsskins_listHiddenSlots(theme=theme)
+
+for hidden_slot in hidden_slots:
+    portlets = ptltool.getPortlets(context, hidden_slot)
+
+    for portlet in portlets:
+        ptltool.deletePortlet(portlet_id=portlet.getId(), context=context)
+
+if REQUEST is not None:
+     url = context.absolute_url() + \
+     '/portlet_manage_form?portal_status_message=' + \
+      cpsmcat('description_unused_porlets_deleted')
+     REQUEST.RESPONSE.redirect(url)
