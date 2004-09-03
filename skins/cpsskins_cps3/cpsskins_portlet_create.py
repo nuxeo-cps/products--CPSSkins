4457baa84fb739804c4ca79b066a2d189593d382
jmorliaguet <devnull@localhost>
2004-09-03 11:35:25 +0000

    passing the 'order' parameter when creating local portlets.

--- skins/cpsskins_cps3/cpsskins_portlet_create.py
+++ skins/cpsskins_cps3/cpsskins_portlet_create.py
@@ -1,9 +1,10 @@
-##parameters=type_name=None, slot=None, REQUEST=None
+##parameters=type_name=None, slot=None, order=None, REQUEST=None
 
 ptltool = context.portal_cpsportlets
 if type_name is not None:
     portlet_id = ptltool.createPortlet(ptype_id=type_name, 
                                        slot=slot, 
+                                       order=order,
                                        context=context)
 
 if REQUEST is not None:
