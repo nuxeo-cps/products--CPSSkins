c1be59695c093ebabb6e7acbfcd8b8cce15cf4a0
jmorliaguet <devnull@localhost>
2004-09-05 23:05:34 +0000

    fixed a typo.
    added permission checks on the boxes too - there is still a case to consider when all order positions are already taken and the user is not allowed to reorder the boxes...

--- skins/cpsskins_cps3/cpsskins_move_portalbox.py
+++ skins/cpsskins_cps3/cpsskins_move_portalbox.py
@@ -9,6 +9,8 @@ portal_path = context.portal_url.getPortalPath() + '/'
 sboxes = btool.getBoxes(context, include_only_in_subfolder=1)
 sboxes = btool.filterBoxes(sboxes, slot=dest_slot, keep_closed=1)
 
+checkPerm = context.portal_membership.checkPermission
+
 if src_slot == dest_slot:
     found = 0
     for box in sboxes: 
@@ -16,7 +18,8 @@ if src_slot == dest_slot:
         if order == dest_ypos and not found:
             found = 1
             box = box['box']
-            box.edit(order=src_ypos)
+            if checkPerm('Manage Boxes', box):
+                box.edit(order=src_ypos)
             if src_ypos == dest_ypos:
                 dest_ypos += 10
             break
@@ -30,7 +33,8 @@ else:
             new_ypos = order + 10
         if found:
             box = box['box']
-            box.edit(order=new_ypos)
+            if checkPerm('Manage Boxes', box):
+                box.edit(order=new_ypos)
             new_ypos += 10
 
 box = context.restrictedTraverse(portal_path + box_url)
