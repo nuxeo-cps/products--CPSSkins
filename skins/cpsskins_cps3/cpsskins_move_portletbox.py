c326560f2aa37f7688d1d775ec5c9ce7e50a737b
jmorliaguet <devnull@localhost>
2004-09-06 00:15:21 +0000

    using the movePortlet() method from CPSPortlets API

--- skins/cpsskins_cps3/cpsskins_move_portletbox.py
+++ skins/cpsskins_cps3/cpsskins_move_portletbox.py
@@ -1,43 +1,13 @@
-##parameters=portlet_rurl=None, src_ypos=None, dest_ypos=None, src_slot=None, dest_slot=None, REQUEST=None
+##parameters=portlet_rurl=None, REQUEST=None, **kw
 
-dest_ypos = int(dest_ypos)
-src_ypos = int(src_ypos)
+if REQUEST is not None:
+    kw.update(REQUEST.form)
 
 portal_path = context.portal_url.getPortalPath() + '/'
+portlet = context.restrictedTraverse(portal_path + portlet_rurl)
 
 ptltool = context.portal_cpsportlets
-portlets = ptltool.getPortlets(context=context, slot=dest_slot, sort=0)
-
-checkPerm = context.portal_membership.checkPermission
-
-if src_slot == dest_slot:
-    found = 0
-    for portlet in portlets: 
-        order = portlet.getOrder()
-        if order == dest_ypos and not found:
-            found = 1
-            if checkPerm('Manage Portlets', portlet):
-                portlet.setOrder(src_ypos)
-            if src_ypos == dest_ypos:
-                dest_ypos += 10
-            break
-else:
-    new_ypos = 0
-    found = 0
-    for portlet in portlets: 
-        order = portlet.getOrder()
-        if order == dest_ypos and not found:
-            found = 1
-            new_ypos = order + 10
-        if found:
-            if checkPerm('Manage Portlets', portlet):
-                portlet.setOrder(new_ypos)
-            new_ypos += 10
-
-portlet = context.restrictedTraverse(portal_path + portlet_rurl)
-if portlet is not None:
-    portlet.setSlot(dest_slot)
-    portlet.setOrder(dest_ypos)
+ptltool.movePortlet(portlet=portlet, context=context, **kw)
 
 if REQUEST is not None:
      url = context.absolute_url() + '/portlet_manage_form'
