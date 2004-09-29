a4acebe893b6cac19dea92db8e4696ae5c7f5e34
jmorliaguet <devnull@localhost>
2004-09-29 07:44:09 +0000

    - removed reference to Localizer - moved  cpsskins_delete_portlet.py from skins/cpsskins_cps3 to skins/cpsskins_cmf - i18n update

--- skins/cpsskins_cmf/cpsskins_delete_portlet.py
+++ skins/cpsskins_cmf/cpsskins_delete_portlet.py
@@ -4,12 +4,10 @@ if portlet_id is None:
     return
 
 ptltool = context.portal_cpsportlets
-cpsmcat = context.Localizer.default
-
 ptltool.deletePortlet(portlet_id, context)
 
 if REQUEST is not None:
+     psm = 'psm_portlet_deleted'
      url = context.absolute_url() + \
-     '/portlet_manage_form?portal_status_message=' + \
-      cpsmcat('description_porlets_deleted')
+     '/portlet_manage_form?portal_status_message=%s' % psm
      REQUEST.RESPONSE.redirect(url)
