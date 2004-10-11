cae267a5ef808d371a8c61376a841eaf72a8bae2
jmorliaguet <devnull@localhost>
2004-10-11 19:55:46 +0000

    added a UI to move portlets between folders.

--- skins/cpsskins_cmf/cpsskins_move_portletbox.py
+++ skins/cpsskins_cmf/cpsskins_move_portletbox.py
@@ -1,4 +1,4 @@
-##parameters=portlet_rurl=None, REQUEST=None, **kw
+##parameters=portlet_rurl=None, src_rurl=None, dest_rurl=None, REQUEST=None, **kw
 
 if REQUEST is not None:
     kw.update(REQUEST.form)
@@ -6,8 +6,19 @@ if REQUEST is not None:
 portal_path = context.portal_url.getPortalPath() + '/'
 portlet = context.restrictedTraverse(portal_path + portlet_rurl)
 
+if src_rurl is not None:
+    src_folder = context.restrictedTraverse(portal_path + src_rurl)
+
+if dest_rurl is not None:
+    dest_folder = context.restrictedTraverse(portal_path + dest_rurl)
+else:
+    dest_folder = src_folder
+
 ptltool = context.portal_cpsportlets
-ptltool.movePortlet(portlet=portlet, context=context, **kw)
+portlet = ptltool.movePortlet(portlet=portlet,
+                              src_folder=src_folder,
+                              dest_folder=dest_folder,
+                              **kw)
 
 context.cpsskins_setViewMode(selected_portlet=portlet.getId())
 
