9900eb8cfb75ca7b2d3281a03b314668e766d6cf
jmorliaguet <devnull@localhost>
2004-10-17 20:21:41 +0000

    bug fix after refactoring.

--- skins/CPSSkins/cpsskins_cellstyler_add.py
+++ skins/CPSSkins/cpsskins_cellstyler_add.py
@@ -1,15 +1,11 @@
 ##parameters=REQUEST=None, **kw
 
-tmtool = context.portal_themes
 if REQUEST is not None:
     kw.update(REQUEST.form)
 
 cellstyler =  context.addCellStyler(**kw)
 if cellstyler is None:
     return
-
-if theme is None:
-    theme = tmtool.getDefaultThemeName()
 
 url = cellstyler.absolute_url() + '/edit_form'
 
