e02545fcd1b486f25c11a0c0799fe711af94243f
jmorliaguet <devnull@localhost>
2004-10-21 09:58:58 +0000

    added support for box slots inside cell blocks - only renderable objects can be placed inside such slots, i.e. portlets so far. - objects depending on macros (CPS3 boxes macros, main content macros) will not be seen.
    TODO: displaying box slots on the portlet manage form.

--- skins/cpsskins_cmf/cpsskins_listHiddenSlots.py
+++ skins/cpsskins_cmf/cpsskins_listHiddenSlots.py
@@ -9,9 +9,21 @@ slots = context.cpsskins_listSlots()
 for block in tmtool.getPageBlocks(theme=theme):
     objects = block.getObjects()
     for cell in objects.keys():
-       for templet in objects[cell]['contents']:
-          if templet.aq_explicit.isPortalBoxGroup():
-              slot = getattr(templet, 'box_group', None)
+
+       for content in objects[cell]['contents']:
+          if getattr(content.aq_explicit, 'isportalboxgroup', 0):
+              slot = content.box_group
               if slot in slots:
                   slots.remove(slot)
+
+          if getattr(content.aq_explicit, 'iscellblock', 0):
+              cell_objects = content.getObjects()
+              for cell2 in cell_objects.keys():
+
+                  for cell_content in cell_objects[cell2]['contents']:
+                      if getattr(cell_content.aq_explicit, 'isportalboxgroup', 0):
+                          slot = cell_content.box_group
+                          if slot in slots:
+                              slots.remove(slot)
+
 return slots
