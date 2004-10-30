05d4a6718a371d2988e3f8dc3c58861e40ffeadb
jmorliaguet <devnull@localhost>
2004-10-30 19:54:33 +0000

    added a redirect url when deleting portlets

--- skins/cpsskins_cmf/cpsskins_delete_portlet.py
+++ skins/cpsskins_cmf/cpsskins_delete_portlet.py
@@ -1,4 +1,4 @@
-##parameters=portlet_id=None, REQUEST=None
+##parameters=portlet_id=None, redirect_url=None, REQUEST=None
 
 if portlet_id is None:
     return
@@ -7,6 +7,8 @@ ptltool = context.portal_cpsportlets
 ptltool.deletePortlet(portlet_id, context)
 
 if REQUEST is not None:
+     if redirect_url is not None:
+         url = redirect_url
      psm = 'psm_portlet_deleted'
      url = context.absolute_url() + \
      '/portlet_manage_form?portal_status_message=%s' % psm
