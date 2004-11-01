14d7942276eee57cb9ae67c35983f4e6c89875aa
jmorliaguet <devnull@localhost>
2004-11-01 22:24:57 +0000

    redesigned the portlet management UI: - using the same UI as CPSSkins's theme editor - added a WYSIWYG panel ( see: http://www.medic.chalmers.se/~jmo/CPS/new_portlet_ui.png ) - added a toolbox to add new portlets using drag and drop - moved the portlet creation form to a popup window - no screen is dependent on master macros's main slot - ...

--- skins/cpsskins_cmf/cpsskins_portlet_add.py
+++ skins/cpsskins_cmf/cpsskins_portlet_add.py
@@ -4,10 +4,7 @@ if REQUEST is not None:
     kw.update(REQUEST.form)
 
 kw['title'] = kw.get('ptype_id', '')
-context.createCPSPortlet(**kw)
+kw['context'] = context
 
-url = context.absolute_url() + '/portlet_manage_form'
-if REQUEST is None:
-    return content
-else:
-    REQUEST.RESPONSE.redirect(url)
+ptltool = context.portal_cpsportlets
+context.createCPSPortlet(REQUEST=REQUEST, **kw)
