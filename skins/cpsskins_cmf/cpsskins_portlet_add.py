47d63f0928f7ef21bde05f48e7599f52bad95ede
jmorliaguet <devnull@localhost>
2004-11-02 13:50:15 +0000

    portlet creation: implemented drag-and-drop from the toolbox.

--- skins/cpsskins_cmf/cpsskins_portlet_add.py
+++ skins/cpsskins_cmf/cpsskins_portlet_add.py
@@ -4,7 +4,18 @@ if REQUEST is not None:
     kw.update(REQUEST.form)
 
 kw['title'] = kw.get('ptype_id', '')
-kw['context'] = context
+
+dest_rurl = kw.get('dest_rurl')
+
+portal_path = context.portal_url.getPortalPath()
+
+dest_folder = None
+if dest_rurl:
+    dest_folder = context.restrictedTraverse(portal_path + '/' + dest_rurl,
+                                             default=None)
+if dest_folder is None:
+    dest_folder = context
+kw['context'] = dest_folder
 
 ptltool = context.portal_cpsportlets
 context.createCPSPortlet(REQUEST=REQUEST, **kw)
