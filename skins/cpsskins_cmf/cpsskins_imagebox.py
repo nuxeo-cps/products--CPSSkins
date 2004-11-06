9e97cec3c9eb35dae844c4bd89c2838a45ff8987
jmorliaguet <devnull@localhost>
2004-11-06 16:12:28 +0000

    using short urls for images

--- skins/cpsskins_cmf/cpsskins_imagebox.py
+++ skins/cpsskins_cmf/cpsskins_imagebox.py
@@ -1,5 +1,7 @@
 ##parameters=REQUEST=None, **kw
 
+REQUEST = context.REQUEST
+
 img = context
 if context.i18n:
     lc = context.cpsskins_getlocalizer(root=1); 
@@ -7,17 +9,17 @@ if context.i18n:
     if current_lang: 
         img_id = 'i18n_image_%s' % current_lang
         img = getattr(context.aq_explicit, img_id, context)
-        
+
+base_url = REQUEST.get('cpsskins_base_url', '')
+
 height = img.height
 width = img.width
 title = img.title
-img_url = img.absolute_url()
 tag = ''
 
+img_url = base_url + context.portal_url.getRelativeUrl(img)
 if context.use_internal_link:
-    if REQUEST is None:
-        REQUEST = context.REQUEST
-    link = REQUEST.get('cpsskins_base_url', '')
+    link = base_url
     internal_link = context.internal_link
     if internal_link != '/':
         link += internal_link
