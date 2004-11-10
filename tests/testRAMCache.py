4ba3e2c4c1209209e4649d8e121066a36207f8c5
jmorliaguet <devnull@localhost>
2004-11-10 09:29:57 +0000

    test update

--- tests/testRAMCache.py
+++ tests/testRAMCache.py
@@ -15,7 +15,7 @@ class TestRAMCache(CPSSkinsTestCase.CPSSkinsTestCase):
         self.pageblock = self.theme_container.addPageBlock()
         self.templet = self.pageblock.addContent(type_name='Text Box Templet')
         self.templet.text = str(range(1000))
-        self.cache = self.templet.getTempletCache(create=1)
+        self.cache = self.templet.getTempletCache()
         self.cache.invalidate()
 
     def test_clearCache(self):
