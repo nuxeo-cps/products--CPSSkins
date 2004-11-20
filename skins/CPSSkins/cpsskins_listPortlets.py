810a326d647d3ffc1207ea21be7b815fa6ae5cf6
jmorliaguet <devnull@localhost>
2004-11-20 20:32:30 +0000

    skip the 'dummy portlet'

--- skins/CPSSkins/cpsskins_listPortlets.py
+++ skins/CPSSkins/cpsskins_listPortlets.py
@@ -2,6 +2,9 @@
 list = []
 
 for ti in context.portal_types.listTypeInfo():
+   # skip the dummy portlet
+   if ti.getId() == 'Dummy Portlet':
+       continue
    if ti.getProperty('cps_is_portlet', 0):
        list.append(ti)
 return list
