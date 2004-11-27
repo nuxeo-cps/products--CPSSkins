f2c606e3be2ed52461d83c506436d5693f23a6dc
jmorliaguet <devnull@localhost>
2004-11-27 16:55:28 +0000

    simplification and taking care of the entire path after '_vh_...'

--- skins/CPSSkins/cpsskins_getBaseUrl.py
+++ skins/CPSSkins/cpsskins_getBaseUrl.py
@@ -16,9 +16,7 @@ if path_info.startswith('/VirtualHostBase/'):
     # Inside-out hosting (VHM _vh_)
     if path_info.find('_vh_') > 0:
         base = path_info.split('_vh_')[1]
-        if base.find('/') > 0:
-            base = base.split('/')[0]
-        return '/' + base + '/'
+        return '/' + base
     else:
         return '/'
 else:
