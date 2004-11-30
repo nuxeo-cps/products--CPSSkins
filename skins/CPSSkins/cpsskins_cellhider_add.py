833b83c7327467610f712469d3fc6e06b87d03a6
jmorliaguet <devnull@localhost>
2004-11-30 22:40:16 +0000

    saving scroll coordinates when adding a cell styler / cell hider

--- skins/CPSSkins/cpsskins_cellhider_add.py
+++ skins/CPSSkins/cpsskins_cellhider_add.py
@@ -3,11 +3,16 @@
 if REQUEST is not None:
     kw.update(REQUEST.form)
 
+tmtool = context.portal_themes
+
 cellhider =  context.addCellHider(**kw)
 if cellhider is None:
     return
 
 url = context.portal_url() + '/cpsskins_theme_manage_form'
+
+# save scroll position
+view_mode = tmtool.setViewMode(**kw)
 
 if REQUEST is None:
     return cellhider
--- skins/CPSSkins/cpsskins_cellstyler_add.py
+++ skins/CPSSkins/cpsskins_cellstyler_add.py
@@ -3,9 +3,14 @@
 if REQUEST is not None:
     kw.update(REQUEST.form)
 
+tmtool = context.portal_themes
+
 cellstyler =  context.addCellStyler(**kw)
 if cellstyler is None:
     return
+
+# save scroll position
+view_mode = tmtool.setViewMode(**kw)
 
 url = context.portal_url() + '/cpsskins_theme_manage_form'
 
