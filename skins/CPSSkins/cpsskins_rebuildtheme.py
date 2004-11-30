a76f95dcade0746359a928526b25f50a224779f7
jmorliaguet <devnull@localhost>
2004-11-30 23:12:29 +0000

    more robust test / acquisition

--- skins/CPSSkins/cpsskins_rebuildtheme.py
+++ skins/CPSSkins/cpsskins_rebuildtheme.py
@@ -1,6 +1,6 @@
 ##parameters=REQUEST=None
 
-if getattr(context, 'isportaltheme', 0):
+if getattr(context.aq_inner.aq_explicit, 'isportaltheme', 0):
     context.rebuild(setperms=1)
 
 if REQUEST is not None:
