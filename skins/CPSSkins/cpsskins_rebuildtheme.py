c0c9f8c4643a7fbfe56a9280492cbe001a3216bd
jmorliaguet <devnull@localhost>
2004-12-05 16:18:55 +0000

    added a portal status message when rebuilding themes

--- skins/CPSSkins/cpsskins_rebuildtheme.py
+++ skins/CPSSkins/cpsskins_rebuildtheme.py
@@ -4,5 +4,6 @@ if getattr(context.aq_inner.aq_explicit, 'isportaltheme', 0):
     context.rebuild(setperms=1)
 
 if REQUEST is not None:
-    url = REQUEST['HTTP_REFERER']
+    url = context.portal_url() + '/cpsskins_theme_manage_form' +\
+          '?portal_status_message=_Theme_rebuilt_'
     REQUEST.RESPONSE.redirect(url)
