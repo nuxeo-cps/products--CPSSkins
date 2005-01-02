d8f3629e14537eddf0f0137504480d1c3c9d985d
jmorliaguet <devnull@localhost>
2005-01-02 15:58:23 +0000

    support for form data passed as a list

--- skins/cpsskins_cmf/cpsskins_copy_to_clipboard.py
+++ skins/cpsskins_cmf/cpsskins_copy_to_clipboard.py
@@ -1,9 +1,13 @@
 ##parameters=object_rurl=None
 
+from types import ListType
+
 if object_rurl is None:
     return
 
-tmtool = context.portal_themes
+if isinstance(object_rurl, ListType):
+    object_rurl = object_rurl[0]
 
+tmtool = context.portal_themes
 # XXX rename 'setViewMode' to something else
 tmtool.setViewMode(clipboard=object_rurl, reload=1)
