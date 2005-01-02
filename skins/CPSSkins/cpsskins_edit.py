7e0e0d964d0108bc248ce6c7fce2a5f781acfd95
jmorliaguet <devnull@localhost>
2005-01-02 15:54:30 +0000

    - setting the REQUEST explicitly to avoid mysterious redirection problems (?) - only templets and cellblocks can be displayed as selected content

--- skins/CPSSkins/cpsskins_edit.py
+++ skins/CPSSkins/cpsskins_edit.py
@@ -1,21 +1,23 @@
-##parameters=REQUEST=None, **kw
+##parameters=cat='', REQUEST=None, **kw
 
-if REQUEST is not None:
-    kw.update(REQUEST.form)
+if REQUEST is None:
+    REQUEST = context.REQUEST
+
+kw.update(REQUEST.form)
 
 tmtool = context.portal_themes
-
 context.edit(**kw)
 
+c = context.aq_inner.aq_explicit
 # category
-cat = kw.get('cat')
-if getattr(context.aq_explicit, 'isportletbox', 0) and kw.get('portlet_type'):
+if getattr(c, 'isportletbox', 0) and kw.get('portlet_type'):
     cat = 'Portlet'
 
 # redirect url
 url = kw.get('redirect_url')
 
-kw['selected_content'] = context.getId()
+if getattr(c, 'isportaltemplet', 0) or getattr(c, 'iscellblock', 0):
+    kw['selected_content'] = context.getId()
 
 # save scroll position
 tmtool.setViewMode(**kw)
@@ -23,7 +25,6 @@ tmtool.setViewMode(**kw)
 if REQUEST is not None:
     if url is None:
         url = context.absolute_url() + '/edit_form'
-
     if cat:
         url += '?cat=' + cat
     REQUEST.RESPONSE.redirect(url)
