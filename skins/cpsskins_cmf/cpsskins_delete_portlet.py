9c50a44eddd56bd8b128b14b0fbec5c4865586f0
jmorliaguet <devnull@localhost>
2005-01-06 13:09:11 +0000

    redirection fixes

--- skins/cpsskins_cmf/cpsskins_delete_portlet.py
+++ skins/cpsskins_cmf/cpsskins_delete_portlet.py
@@ -1,4 +1,4 @@
-##parameters=portlet_id=None, redirect_url=None, REQUEST=None
+##parameters=portlet_id=None, redirect_rurl=None, REQUEST=None
 
 if portlet_id is None:
     return
@@ -7,8 +7,10 @@ ptltool = context.portal_cpsportlets
 ptltool.deletePortlet(portlet_id, context)
 
 if REQUEST is not None:
-     if redirect_url is None:
+     if redirect_rurl is None:
          redirect_url = context.absolute_url()
+     else:
+         redirect_url = context.portal_url() + '/' + redirect_rurl
      psm = 'psm_portlet_deleted'
      url = redirect_url + \
      '/portlet_manage_form?portal_status_message=%s' % psm
--- skins/cpsskins_cmf/cpsskins_duplicate_portlet.py
+++ skins/cpsskins_cmf/cpsskins_duplicate_portlet.py
@@ -1,4 +1,4 @@
-##parameters=portlet_id=None, redirect_url=None, REQUEST=None
+##parameters=portlet_id=None, redirect_rurl=None, REQUEST=None
 
 if portlet_id is None:
     return
@@ -7,7 +7,9 @@ ptltool = context.portal_cpsportlets
 ptltool.duplicatePortlet(portlet_id, context)
 
 if REQUEST is not None:
-     if redirect_url is None:
+     if redirect_rurl is None:
          redirect_url = context.absolute_url()
+     else:
+         redirect_url = context.portal_url() + '/' + redirect_rurl
      redirect_url += '/portlet_manage_form'
      REQUEST.RESPONSE.redirect(redirect_url)
