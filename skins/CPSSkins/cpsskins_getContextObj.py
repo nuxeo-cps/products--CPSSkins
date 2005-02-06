0a91ca651d0d124b80470ae8cdd46f11d162d23a
jmorliaguet <devnull@localhost>
2005-02-06 22:08:25 +0000

    optimizations: - here/cpsskins_lib/macros/portalboxgroup is computed only once. - using 'default=None' on restrictedTraverse() instead of try: except:

--- skins/CPSSkins/cpsskins_getContextObj.py
+++ skins/CPSSkins/cpsskins_getContextObj.py
@@ -5,10 +5,7 @@ if context_rurl is not None:
     portal_url = context.portal_url(relative=1)
     if portal_url != '/':
         portal_url = '/' + portal_url
-    try:
-        context_obj = context.restrictedTraverse(portal_url + context_rurl)
-    except KeyError:
-        pass
+    context_obj = context.restrictedTraverse(portal_url + context_rurl, default=None)
 
 if context_obj is None:
     context_obj = context.REQUEST.get('context_obj', context)
