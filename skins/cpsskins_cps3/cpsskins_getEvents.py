628d92cc8d9f782ab25be812b5cc47706379d638
jmorliaguet <devnull@localhost>
2005-02-08 22:09:50 +0000

    fix: the 'start' field is not indexed by the catalog so we have to get the content of the proxy.

--- skins/cpsskins_cps3/cpsskins_getEvents.py
+++ skins/cpsskins_cps3/cpsskins_getEvents.py
@@ -1,19 +1,20 @@
-results = context.portal_catalog.searchResults(review_state='published',
-                                               end={'query': context.ZopeTime(), 'range': 'min'},
-                                               sort_on='start',
-                                              )[:5]
+results = context.portal_catalog.searchResults(
+    review_state='published',
+    end={'query': context.ZopeTime(), 'range': 'min'},
+    sort_on='start',
+    )[:5]
 
 events = []
 for res in results:
     if hasattr(res.aq_explicit, 'getRID'):
         res = res.getObject()
-    events.append({
-                    'url': res.absolute_url(),
-                    'title': res.title_or_id(),
-                    'icon': res.getIcon(),
-                    'location': getattr(res, 'location', ''),
-                    'start': DateTime(res.start().strftime('%d/%m/%y'))
-                  }
-                 )
+    event = res.getContent()
+    start = getattr(event, 'start', None)
+    events.append({'url': event.absolute_url(),
+                   'title': event.title_or_id(),
+                   'icon': event.getIcon(),
+                   'location': '',
+                   'start': start and DateTime(start.strftime('%m/%d/%y')).ISO() or '',
+                  })
 return events
 
