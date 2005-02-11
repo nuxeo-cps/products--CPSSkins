a8027605de841be64962ed4dc848d121a758eaf2
jmorliaguet <devnull@localhost>
2005-02-11 11:44:59 +0000

    optimization: we do not need to getContent() to obtain the event's start date.

--- skins/cpsskins_cps3/cpsskins_getEvents.py
+++ skins/cpsskins_cps3/cpsskins_getEvents.py
@@ -6,15 +6,13 @@ results = context.portal_catalog.searchResults(
 
 events = []
 for res in results:
+    event_start = res.start
     if hasattr(res.aq_explicit, 'getRID'):
         res = res.getObject()
-    event = res.getContent()
-    start = getattr(event, 'start', None)
     events.append({'url': res.absolute_url(),
                    'title': res.title_or_id(),
                    'icon': res.getIcon(),
                    'location': '',
-                   'start': start and start.ISO() or '',
+                   'start': event_start.ISO(),
                   })
 return events
-
