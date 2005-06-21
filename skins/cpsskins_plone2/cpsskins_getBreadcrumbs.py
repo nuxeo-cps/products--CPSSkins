7348faa5472883953cb70216e080d4e3d780f456
jmorliaguet <devnull@localhost>
2005-06-21 20:11:53 +0000

    - misc Plone2.1 fixes (breadcrumbs, new panel template)

--- skins/cpsskins_plone2/cpsskins_getBreadcrumbs.py
+++ skins/cpsskins_plone2/cpsskins_getBreadcrumbs.py
@@ -1,12 +1,20 @@
 
-bcs = context.breadcrumbs()
-
 result = []
-for name, url in bcs:
-    result.append( { 'id': name,
-                     'title': name,
-                     'url': url
-                   }
-                 )
-return result
 
+# Plone 2.0.x
+breadcrumbs = getattr(context, 'breadcrumbs', None)
+if breadcrumbs is not None and callable(breadcrumbs):
+    for name, url in breadcrumbs():
+        result.append({'id': name,
+                        'title': name,
+                        'url': url,
+                      })
+# Plone 2.1
+else:
+    for bc in context.plone_utils.createBreadCrumbs(context):
+        name = bc['Title']
+        result.append({'id': name,
+                       'title': name,
+                       'url': bc['absolute_url'],
+                      })
+return result
