39a64bd137540925d4d3eff871cc855b4a35c53f
jmorliaguet <devnull@localhost>
2005-07-26 09:39:31 +0000

    - forgot to store the url in the cache

--- skins/CPSSkins/cpsskins_getBaseUrl.py
+++ skins/CPSSkins/cpsskins_getBaseUrl.py
@@ -10,13 +10,14 @@ if REQUEST is not None and REQUEST.has_key('cpsskins_base_url'):
 # use utool.getBaseUrl() if it is present
 utool = context.portal_url
 if getattr(utool.aq_inner.aq_explicit, 'getBaseUrl', None) is not None:
-    return utool.getBaseUrl()
+    base_url= utool.getBaseUrl()
 
-# CMF, CPS < 3.3.5, Plone ...
-portal = utool.getPortalObject()
-base_url = portal.absolute_url_path()
-if not base_url.endswith('/'):
-    base_url += '/'
+else:
+    # CMF, CPS < 3.3.5, Plone ...
+    portal = utool.getPortalObject()
+    base_url = portal.absolute_url_path()
+    if not base_url.endswith('/'):
+        base_url += '/'
 
 # cache the base url in the REQUEST
 if REQUEST is not None:
