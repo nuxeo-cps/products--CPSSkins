aac02795c6b81b5c4632dd08f02b757f73ef3773
jmorliaguet <devnull@localhost>
2005-11-30 19:52:31 +0000

    - added import / export of themes tool properties (90% done)

--- __init__.py
+++ __init__.py
@@ -332,6 +332,7 @@ def initialize(registrar):
                 'default', 'CPSSkins', 'CPSSkins installation profile.',
                 'profiles/default', 'CPSSkins', EXTENSION)
 
+        if 'CPSSkins:cps3' not in profile_registry.listProfiles():
             profile_registry.registerProfile(
                 'cps3', 'CPS3 themes', 'CPS3 themes for a CPSSkins-based site',
                 'profiles/cps3', 'CPSSkins', EXTENSION)
--- setup/handlers.py
+++ setup/handlers.py
@@ -1,41 +1,93 @@
+# Copyright (c) 2003-2005 Chalmers University of Technology
+# Authors: Jean-Marc Orliaguet <mailto:jmo@ita.chalmers.se>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 2 as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+
+__author__ = "Jean-Marc Orliaguet <jmo@ita.chalmers.se>"
 
 import os
 
-from Globals import InitializeClass
-from Globals import package_home
+from AccessControl import ClassSecurityInfo
+from Globals import InitializeClass, package_home
 
 from Products.CMFCore.utils import getToolByName
+from Products.CMFCore.permissions import ManagePortal
 from Products.CMFSetup.utils import ExportConfiguratorBase
 from Products.CMFSetup.utils import ImportConfiguratorBase
+from Products.CMFSetup.utils import KEY
 from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 
 _pkgdir = package_home(globals())
 _xmldir = os.path.join(_pkgdir, 'xml')
 
-_FILENAME = 'themes.xml'
+_TOOL_FILENAME = 'themestool.xml'
+_THEMES_FILENAME = 'themes.xml'
 
-#######################################################################
+#
 # Handlers
-#######################################################################
+#
 
 def importThemesTool(context):
     """Import the theme tool's properties.
     """
+    site = context.getSite()
+    tmtool = getToolByName(site, 'portal_themes')
+    if context.shouldPurge():
+        # steps to follow to remove old settings
+        pass
+
+    body = context.readDataFile(_TOOL_FILENAME)
+    if body is None:
+        return 'Themes tool: Nothing to import.'
+
+    ttic = ThemesToolImportConfigurator(site)
+    tm_info = ttic.parseXML(body)
+
+    tmtool.accesskey = tm_info['accesskey']
+    tmtool.debug_mode = int(tm_info['debug_mode'])
+
+    # TODO: convert to list / dicts
+    #tmtool.externalthemes = tm_info['externalthemes']
+    #tmtool.method_themes = tm_info['method_themes']
+
+    return 'Themes tool settings imported.'
 
 def exportThemesTool(context):
     """Export the theme tool's properties.
     """
+    site = context.getSite()
+    ttec = ThemesToolExportConfigurator(site).__of__(site)
+    text = ttec.generateXML()
+    context.writeDataFile(_TOOL_FILENAME, text, 'text/xml' )
+
+    return 'Theme tool settings exported.'
 
 def importPortalThemes(context):
     site = context.getSite()
     tmtool = getToolByName(site, 'portal_themes')
     ttic = ThemeImportConfigurator(site).__of__(site)
 
-    xml = context.readDataFile(_FILENAME)
+    if context.shouldPurge():
+        # steps to follow to remove old settings
+        pass
+
+    xml = context.readDataFile(_THEMES_FILENAME)
     if xml is None:
         return 'Portal themes: Nothing to import.'
 
-    #TODO
     print xml
     return 'Portal themes imported'
 
@@ -49,34 +101,66 @@ def exportPortalThemes(context):
     tec = ThemeExportConfigurator(site).__of__(site)
     themes_xml = tec.generateXML()
 
-    context.writeDataFile(_FILENAME, themes_xml, 'text/xml')
+    context.writeDataFile(_THEMES_FILENAME, themes_xml, 'text/xml')
 
     for theme in themes:
         theme_id = theme.getId()
         theme_filename = '%s.xml' % theme_id.replace(' ', '_')
         theme_xml = tec.generateXML(theme_id=theme_id)
         context.writeDataFile(theme_filename, theme_xml, 'text/xml', 'themes')
+
     return 'Portal themes exported'
 
-#######################################################################
+#
 # Configurators
-#######################################################################
+#
 
 class ThemesToolImportConfigurator(ImportConfiguratorBase):
-    """ """
+
+    def _getImportMapping(self):
+        return {
+          'object': {
+              'accesskey': {},
+              'debug_mode': {},
+              'externalthemes': {},
+              'method_themes': {}
+              }
+          }
+
+InitializeClass(ThemesToolImportConfigurator)
+
 
 class ThemesToolExportConfigurator(ExportConfiguratorBase):
-    """ """
+    security = ClassSecurityInfo()
+
+    security.declareProtected(ManagePortal, 'getToolInfo')
+    def getToolInfo( self ):
+        """Get information about the tool
+        """
+        tmtool = getToolByName(self._site, 'portal_themes')
+        config = {}
+        config['accesskey'] = tmtool.getAccessKey()
+        config['debug_mode'] = tmtool.debug_mode
+        config['externalthemes'] = tmtool.getExternalThemes()
+        config['method_themes'] = tmtool.method_themes
+        return config
+
+    def _getExportTemplate(self):
+        return PageTemplateFile('tmtoolExport.xml', _xmldir)
+
+InitializeClass(ThemesToolExportConfigurator)
+
 
 class ThemeImportConfigurator(ImportConfiguratorBase):
     """ """
 
+InitializeClass(ThemeImportConfigurator)
+
+
 class ThemeExportConfigurator(ExportConfiguratorBase):
-    """ """
+    security = ClassSecurityInfo()
 
-    def _propertyMap(self):
-        return {}
-
+    security.declareProtected(ManagePortal, 'listThemeObjects')
     def listThemeObjects(self, theme_id):
         """Return a sequence of mappings for theme objects.
         """
@@ -84,6 +168,7 @@ class ThemeExportConfigurator(ExportConfiguratorBase):
         theme = tmtool.getThemeContainer(theme=theme_id)
         return self._extractObject(theme)['subobjects']
 
+    security.declareProtected(ManagePortal, 'getThemes')
     def getThemes(self):
         """Return the list of themes.
         """
@@ -92,8 +177,8 @@ class ThemeExportConfigurator(ExportConfiguratorBase):
     def _getExportTemplate(self):
         return PageTemplateFile('themeExport.xml', _xmldir)
 
+    def _propertyMap(self):
+        return {}
 
-InitializeClass(ThemesToolImportConfigurator)
-InitializeClass(ThemesToolExportConfigurator)
-InitializeClass(ThemeImportConfigurator)
 InitializeClass(ThemeExportConfigurator)
+
