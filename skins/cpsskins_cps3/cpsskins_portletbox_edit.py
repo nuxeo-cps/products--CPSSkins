2b9e87532b2ad5fbc07ffaad85922e74047e92ab
jmorliaguet <devnull@localhost>
2006-01-20 15:20:31 +0000

    - expire the cache entries when the portlet box templet is updated.

--- skins/cpsskins_cps3/cpsskins_portletbox_edit.py
+++ skins/cpsskins_cps3/cpsskins_portletbox_edit.py
@@ -6,6 +6,11 @@ doc = context.getContent()
 is_valid, ds = doc.validate(request=REQUEST, proxy=context, cluster=cluster,
                             use_session=True)
 
+# update cache parameters
+ptype_id = doc.getPortletType()
+doc.resetCacheTimeout()
+doc.resetInterestingEvents(ptype_id)
+doc.expireCache()
 
 url = REQUEST['HTTP_REFERER']
 REQUEST.RESPONSE.redirect(url)
