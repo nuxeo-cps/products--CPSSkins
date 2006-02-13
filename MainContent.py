5fd88609d8cdc89310c9e90dc329e3684af778e6
jmorliaguet <devnull@localhost>
2006-02-13 17:41:06 +0000

    - reverted until a better fix is found

--- MainContent.py
+++ MainContent.py
@@ -67,7 +67,7 @@ class MainContent(BaseTemplet):
     def isRenderable(self):
         """Returns true if the Templet can be rendered.
         """
-        return 1
+        return None
 
     security.declarePublic('isMainContent')
     def isMainContent(self):
